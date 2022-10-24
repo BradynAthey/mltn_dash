7cc08d7dadb28e35fa320d4a93ecdfcb90ec47ff
Craig Corcoran <craig.corcoran@siriusxm.com>
2022-10-24 14:00:38 -0500

    - move graph and image components to separate components.py file

--- apps/mltn_app.py
+++ apps/mltn_app.py
@@ -1,30 +1,13 @@
-import datetime as dt
+import json
 import logging
-
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_cytoscape as cyto
-import json
+from .components import get_cyto_graph, get_static_image
 
-
-# import matplotlib
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import pandas as pd
-# import plotly.express as px
-# import plotly.graph_objs as go
-# import plotly.io as pio
-# from dash.dependencies import Input, Output, State
-
-# """""""""""""""""""""""""""""""""" Initialization """""""""""""""""""""""""""""""""""
 logger = logging.getLogger(__name__)
-# plt.style.use("seaborn-whitegrid")
-# pio.templates.default = "plotly_white"
 
-
-# """""""""""""""""""""""""""""""""" App Boilerplate """""""""""""""""""""""""""""""""""
 # external js scripts
 external_scripts = [
     dict(
@@ -54,144 +37,22 @@ app = dash.Dash(
     external_scripts=external_scripts,
     external_stylesheets=external_stylesheets,
 )
-# server = app.server  # expose for wsgi
-
-# A hidden div where data can be stored and passed between callbacks
-data_store = html.Div(id="data-store", style={"display": "none"})
-
-# """""""""""""""""" Dash Components (to be arranged in the layout) """""""""""""""""""
 
 data_map_tab = dcc.Tab(
     label="Data Map",
-    children=html.Img(src="/assets/images/graph.png", width="100%"),
+    children=get_static_image("/assets/images/graph.png"),
 )
-
-with open("apps/assets/graph-elements.json", "r") as f:
-    graph_elements = json.load(f)
 
 ontology_tab = dcc.Tab(
     label="Ontology",
-    children=cyto.Cytoscape(
-        id="ontology-graph",
-        layout={"name": "circle", "directed": "true", "radius": 1000},
-        style={"height": "80vh", "width": "100%"},
-        minZoom=0.1,
-        maxZoom=1,
-        elements=graph_elements,
-        stylesheet=[
-            {
-                "selector": "node",
-                "style": {
-                    "label": "data(label)",
-                    "width": "350%",
-                    "height": "350%",
-                    "text-wrap": "wrap",
-                    "text-valign": "center",
-                    "text-halign": "center",
-                    "text-max-width": "60px",
-                    "color": "white",
-                    "font-size": "55px",
-                },
-            },
-            {
-                "selector": "edge",
-                "style": {
-                    "width": "3px",
-                    "target-arrow-shape": "triangle",
-                    "source-arrow-shape": "triangle",
-                    "curve-style": "bezier",
-                    "label": "data(label)",
-                    "edge-text-rotation": "autorotate",
-                    "font-size": "40px",
-                    "control-point-step-size": "100px",
-                },
-            },
-        ],
-    ),
+    children=get_cyto_graph("apps/assets/graph-elements.json", id="ontology-graph"),
 )
-
 
 app.layout = html.Div(
     dcc.Tabs(children=[data_map_tab, ontology_tab]),
     id="app-container",
     className="flex-container",
 )
-
-# """""""""""""""""""""""""""""""""" Input Components """"""""""""""""""""""""""""""""""
-# # dropdown input
-# dropdown_options_dict = {
-#     "Option A": "option_a",
-#     "Option B": "option_b",
-#     "Option C": "option_c",
-# }
-# dropdown_options_val_to_name = {v: k for k, v in dropdown_options_dict.items()}
-# dropdown_options = [
-#     dict(label=name, value=val) for name, val in dropdown_options_dict.items()
-# ]
-# dropdown = dcc.Dropdown(
-#     id="dropdown",
-#     options=dropdown_options,
-#     className="f-row",
-#     value="option_a",
-#     # multi=True, # allow multi-select
-# )
-
-# dropdown_card = html.Div(
-#     [html.Div("Options:", className="f-row f-label"), dropdown],
-#     id="dropdown-row",
-#     className="f-column card",
-# )
-
-# date_range = dcc.DatePickerRange(
-#     id="date-range",
-#     min_date_allowed=dt.datetime(2022, 1, 1),
-#     max_date_allowed=dt.datetime(2025, 1, 1),
-#     start_date=dt.datetime(2022, 9, 1),
-#     end_date=dt.datetime(2022, 9, 7),
-# )
-
-# date_range_card = html.Div(
-#     [html.Div("Date Range:", className="f-row f-label"), date_range],
-#     className="f-column card",
-#     id="date-range-row",
-# )
-
-# run_button = html.Div(
-#     html.Button("Run", id="run-button", className="btn btn-outline-primary"),
-#     className="f-row",
-# )
-# run_button_card = html.Div(run_button, id="run-button-card", className="f-row card")
-
-# # """""""""""""""""""""""""""""""""" Output Components """"""""""""""""""""""""""""""""
-
-# mpl_figure = html.Img(id="mpl-figure")
-# # Loading object creates loading animation while output is being computed
-# mpl_figure_loading = dcc.Loading(mpl_figure, id="mpl-figure-loading", type="circle")
-# mpl_figure_card = html.Div(
-#     mpl_figure_loading, id="mpl-figure-card", className="f-row card"
-# )
-
-# plotly_figure = dcc.Graph(
-#     id="plotly-figure"
-# )  # generic container for plotly figure/chart
-# plotly_figure_loading = dcc.Loading(
-#     plotly_figure, id="plotly-figure-loading", type="circle"
-# )
-# plotly_figure_card = html.Div(
-#     plotly_figure_loading, id="plotly-figure-card", className="f-row card"
-# )
-
-
-# """""""""""""""""""""""""""""""""" Component Layout """"""""""""""""""""""""""""""""""
-
-# input_row = html.Div(
-#     [date_range_card, run_button_card], id="input-row", className="f-row"
-# )
-# app.layout = html.Div(
-#     [dropdown_card, input_row, plotly_figure_loading, mpl_figure_loading],
-#     id="app-container",
-#     className="flex-container",
-# )
 
 
 if __name__ == "__main__":
