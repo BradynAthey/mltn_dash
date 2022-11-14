d79bad08b224ca0d7d76899ac74af8836598d7fe
BradynAthey <bradyn.athey@valkyrie.ai>
2022-11-14 10:23:43 -0600

    Re-do with css grid

--- apps/welcome.py
+++ apps/welcome.py
@@ -7,42 +7,45 @@ from dash import html
 welcome_text = 'Welcome to the ML Transition Package powered by Valkyrie. \n'
 about_text = """\n You're here because you know your data has untapped potential,
                 and we're here to help you unlock it. \n
-                The tabls above represent the deliverables from this phase of work.
+                The tabs above represent the deliverables from this phase of work.
                 Click through to explore a preview of the tools our team will build for you during our engagement. \n
                 Should you have any questions, as always, please feel free to reach out to our team \n """ 
 
-title = html.H1(children='Machine Learning Transition Toolkit',className='header-title')
+#title = html.H1(children='Machine Learning Transition Toolkit'),#className='header-title')
 description = html.P(children = 'Client Name')
 
-title  = dbc.Row([
-                html.Div(title),
-                 ])
-text = dbc.Row([
-            dbc.Col([
-                html.Strong([welcome_text]),
+# title  = dbc.Row([
+#                 html.Div(title),
+#                  ])
+# text = dbc.Row([
+#             dbc.Col([
+#                 html.Strong([welcome_text]),
+#                 html.P([about_text])
+#                 ],
+#              className = 'textarea'
+#             )
+#         ])
+
+# welcome_page = html.Div([
+#         # dbc.Row([
+#         #         dbc.Col([
+#         #                 html.Div(title),
+#         #                 #html.Div(text),
+#         # ]),
+#         dbc.Row([
+#                 dbc.Col([
+#                          html.Div(text),
+#                 # ]),
+#                 # dbc.Col([
+#                 #         html.Img(src="/assets/valkyrie_intro_page.jpg")#,height='800px',width = '650px')
+#                 ],)#style = {'margin-top':'10px'})
+#         ])
+# ])
+
+layout = html.Div([html.Strong([welcome_text]),
                 html.P([about_text])
                 ],
-             className = 'textarea'
-            )
-        ])
-
-welcome_page = html.Div([
-        dbc.Row([
-                dbc.Col([
-                        html.Div(title),
-                        #html.Div(text),
-        ]),
-        dbc.Row([
-                dbc.Col([
-                         html.Div(text),
-                ]),
-                dbc.Col([
-                        html.Img(src="/assets/valkyrie_intro_page.jpg",height='800px',width = '650px')
-                ],style = {'margin-top':'10px'})
-        ])
-])])
-
-layout = html.Div([welcome_page])#,className = 'welcome-background')
+             className = 'text-area')
         # style= {'background-image':"url('/assets/network1.jpg')",
         # 'background-size': 'cover',
         # 'background-position':'center',
--- index.py
+++ index.py
@@ -19,17 +19,17 @@ HOST_ADDRESS = CONFIG['HOST_ADDRESS']
 PORT_NUMBER = CONFIG['PORT_NUMBER']
 
 
-banner_image = html.Img(src="/assets/valkyrie_slogan.png",height='70px',width='95%')
-#title = html.H1(children='Machine Learning Transition Toolkit')
+#banner_image = html.Img(src="/assets/valkyrie_slogan.png",height='70px',width='95%')
+title = html.H1(children='Machine Learning Transition Toolkit')
 #description = html.P(children = 'Client Name')
 navbar = dbc.Navbar(
-            dbc.Container([
-                dbc.Row([
-                        dbc.Col([
-                            banner_image
-                        ])
-                    ]),
-                    dbc.Row([ 
+            # dbc.Container([
+                # dbc.Row([
+                #         dbc.Col([
+                #             banner_image
+                #         ])
+                #     ]),
+                    # dbc.Row([ 
                         dbc.Nav([ 
                             dbc.NavItem(dbc.NavLink("Welcome", href="/welcome",disabled=False)),
                             dbc.NavItem(dbc.NavLink("Data Map", href="/data-map",disabled=False)),
@@ -38,18 +38,25 @@ navbar = dbc.Navbar(
                             dbc.NavItem(dbc.NavLink("Data & ML Readiness", href="/data-ml-readiness",disabled=False)),
                             dbc.NavItem(dbc.NavLink("ROI Assessment", href="/roi-assessment",disabled=False)),
                             dbc.NavItem(dbc.NavLink("Roadmap", href="/roadmap",disabled=False)),
-                        ])
-                    ])
+                        # ])
+                    #])
                 ]
-            ),style = {"width":"100%", "height":"200px",'border':'2px solid black'}
+            ),className = 'navbar'#style = {"width":"100%", "height":"200px",'border':'2px solid black'}
 )
        
-app.layout = dbc.Container([
-            dcc.Location(id='url', refresh=False),
-        navbar,
-       html.Div(id = 'page-content-container')
-],fluid=False)
+app.layout =dbc.Container([
+    dcc.Location(id='url', refresh=False),
+    html.Div(title,className = 'header-title'),
+    html.Div(navbar,className='navbar'),
+    html.Div(id = 'page-content-container')
+    ],
+                className = 'grid-container')
 
+#dbc.Container([
+#             dcc.Location(id='url', refresh=False),
+#         navbar,
+#        html.Div(id = 'page-content-container')
+# ],fluid=False,className='grid-container' )
 
 @app.callback(Output('page-content-container', 'children'),
               [Input('url', 'pathname')])
